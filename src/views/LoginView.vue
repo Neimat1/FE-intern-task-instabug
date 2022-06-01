a6433fccf7850ee0ec1e01ac460942d345d14400
Neimat1 <neimat.soliman.ismail@gmail.com>
2022-06-01 23:15:20 +0200

    add loginView page

---
+++ src/views/LoginView.vue
@@ -0,0 +1,31 @@
+<template >
+    <div class="d-flex">
+        <carousel class="w-50" :slides="slides"></carousel>
+        <login-form class="w-50"/>
+    </div>
+</template>
+
+<script>
+    import Carousel from '../components/layout/carousel/CarouselComponent.vue'
+    import LoginForm from '@/components/layout/login/LoginForm.vue'
+    export default {
+        components:{Carousel,LoginForm},
+        data:()=>({
+            slides:[
+                {
+                    img:'https://svgshare.com/i/hrA.svg',
+                    title:'Accelerate Your Entire Mobile Team Workflow'
+                },
+                {
+                    img:'https://svgshare.com/i/hrB.svg',
+                    title:'The Most Comprehansive Bug Reporting Tool for Mobile Apps'
+                },
+                {
+                    img:'https://svgshare.com/i/hrV.svg',
+                    title:'Secure Crash Reporting With Real-Time Alerts'
+                }
+            ]
+        })
+    }
+    
+</script>

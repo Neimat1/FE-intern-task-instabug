0db663aa9bc1788a127b205b6ffa60c1458d670f
Neimat1 <neimat.soliman.ismail@gmail.com>
2022-06-01 23:17:08 +0200

    update router folder with login path

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from '../views/LoginView.vue'
 
 const routes = [
- 
+  {
+    path: '/login',
+    name: 'login',
+    meta:{
+      title:'Login | Instabug'
+    },
+    component: LoginView
+  },
 ]
 
 const router = createRouter({

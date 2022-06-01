3c8fe5614245957be53ae3dcfb5f5d670fb30d19
Neimat1 <neimat.soliman.ismail@gmail.com>
2022-06-01 23:20:01 +0200

    add 404View page

---
+++ src/views/404View.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="not-found">
+        <h1>404 - page not found!</h1>
+        <p>Sorry, that page doesn't exist. What would you like to do?</p>
+    </div>
+</template>
+
+<style src="../assets/SCSS/style.scss" lang="scss" >
+</style>

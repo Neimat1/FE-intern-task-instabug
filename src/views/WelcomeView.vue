1227dc9c0604fd4bc42729e29b14e8d76adcf811
Neimat1 <neimat.soliman.ismail@gmail.com>
2022-06-01 23:22:53 +0200

    add welcomeView page

---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,9 @@
+<template>
+    <Navbar></Navbar>
+</template>
+<script>
+    import Navbar from "../components/layout/navbar/NavbarComponent.vue";
+    export default{
+        components:{Navbar},
+    }
+</script>
